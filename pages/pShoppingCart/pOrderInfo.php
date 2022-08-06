b3694bc7a8aa61c4631daa59e2d8462488ae0aad
amoko2021 <83492837+amoko2021@users.noreply.github.com>
2022-08-06 20:55:32 +0700

    Add files via upload

---
+++ pages/pShoppingCart/pOrderInfo.php
@@ -0,0 +1,64 @@
+<div class="orderInfo container">
+    <h1 class="text-center">Thông tin giao hàng</h1><br>
+    <form class="form-horizontal" name="frmTaoThongTinKhachHang" action="index.php?c=103" method="post">
+        <fieldset>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="txtHoTen">Họ và Tên</label>  
+        <div class="col-md-4">
+        <input id="txtHoTen" name="txtHoTen" type="text" placeholder="Họ và Tên" class="form-control input-md" required>
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="txtSoDienThoai">Số điện thoại</label>  
+        <div class="col-md-4">
+        <input id="txtSoDienThoai" name="txtSoDienThoai" type="number" placeholder="Số điện thoại" class="form-control input-md" required>
+        </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="txtDiaChi">Địa chỉ</label>  
+        <div class="col-md-4">
+        <input id="txtDiaChi" name="txtDiaChi" type="text" placeholder="Địa chỉ" class="form-control input-md" required>
+        </div>
+        </div>
+
+        <!-- Button -->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for=""></label>
+        <div class="col-md-4">
+            <button type="submit" class="btn btn-success">Tạo thông tin và Đặt hàng</button>
+        </div>
+        </div>
+
+        </fieldset>
+    </form>
+    <hr style="border-top: 1px solid #ccc;" /><br>
+    <form class="form-horizontal" name="frmDatHangQuaMaKhachHang" action="index.php?c=104" method="post">
+        <fieldset>
+
+        <!-- Text input-->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for="textinput">Nhập mã giảm giá</label>  
+        <div class="col-md-4">
+        <input id="textinput" name="txtMaKhachHang" type="text" placeholder="Mã giảm giá" class="form-control input-md" required="">
+            
+        </div>
+        </div>
+
+        <!-- Button -->
+        <div class="form-group">
+        <label class="col-md-4 control-label" for=""></label>
+        <div class="col-md-4">
+            <button type="submit" class="btn btn-success">Đặt hàng</button>
+        </div>
+        </div>
+
+        </fieldset>
+        </form>
+        <a class="1234" href="http://Youtube.com">Bấm vào đây để xem</a>
+</div>
